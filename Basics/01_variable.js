46ce80a35efe9963ebb6edcbd31e5b301d474e11
sasanironil <126694847+sasanironil@users.noreply.github.com>
2023-03-01 16:56:38 +0000

    conversion of datatype

---
+++ Basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 3579 
+let accountEmail = "rsasani@gmail.com"
+var accountPassword = "12345"
+accountCity = "surat"
+let accountState;
+// accountId = "32" // we can't change the id if it is in the const variable
+
+accountEmail = "rsa@gmail.com"
+accountPassword = "54123"
+accountCity = "gujrat"
+
+console.log(accountId);
+
+console.table([accountId,  accountEmail, accountPassword, accountCity, accountState])
---
+++ Basics/02_datatype.js
@@ -0,0 +1,16 @@
+"Use Strict"; // Treat as a newer version of js 
+
+let name ="ronil"
+let age = "19"
+let isLoggedIn = "false"
+
+// datatypes
+// number  2 to power 53
+// bigint // use for the bigf numbers now often somtimes we use
+// string // for name or long string
+// boolean // true or false // only this two values foe ex if we want to see that user is login then it is true otherwise false
+// null // standalone value 
+// undifined // it means that value is not defined
+// symbol // it means that we can set the unique value
+
+console.log(typeof undefined); // typeof is use for if we want to check the value of the current enter value or object opr datatype
