cd15cdb7b8547637b7d62257c3543719e9c5b7a1
sasanironil <126694847+sasanironil@users.noreply.github.com>
2023-03-02 15:04:16 +0000

    dataype summary

---
+++ Basics/03_converstionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)// we specified the score as a number or it will convert into number
+console.log(valueInNumber);
+
+// "33" // it is easy to convert in number
+// "33abc" // it is not easy it shows NaN which means Not A Number
+// true // it shows the 1 which means it is boolean value If it is False then it shows the 0
+
+// conver the numbers in to boolean
+
+let isLoggedIn = 1
+ let booleandIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleandIsLoggedIn);
+
+ // if we write 1 then it shows the true
+ // if write o then it shows the false
+ // it we write "" then it shows the false
+ // if we write "ronil" then it shows the true
+
+ 
